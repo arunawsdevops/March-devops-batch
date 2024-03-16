18bfdc05d3626a876c58a1e952cfbe537f501ebe
arunawsdevops <arunawscloud53@gmail.com>
2024-03-16 21:21:23 +0530

    Adding terra code for variables

--- main.tf
+++ main.tf
@@ -1,8 +1,14 @@
-resource "aws_s3_bucket" "bucket-example" {
-  bucket = arun-123-bsoft-test
+resource "aws_instance" "ec2_example" {
 
-  tags = {
-    Name        = "My bucket"
-    Environment = "Dev"
-  }
+   ami                         =  var.ami_id
+   instance_type               =  var.instance_type
+   count                       =  var.instance_count
+   associate_public_ip_address =  var.enable_public_ip
+   
+   tags = var.project_environment
+}
+
+resource "aws_iam_user" "example" {
+  count = length(var.user_names)
+  name  = var.user_names[count.index]
 }
--- provider.tf
+++ provider.tf
@@ -9,5 +9,5 @@ terraform {
 
 provider "aws" {
   region = "us-east-1"
-  profile = "terraform-project-dev"
+  profile = var.profile_name
 }
