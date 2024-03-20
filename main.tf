5c01fae88515f62d0be375c0756503575ca7adc8
PranavTDevops <pranavt.mail@gmail.com>
2024-03-20 22:41:41 +0545

    Terraform using Tfvars

---
+++ main.tf
@@ -0,0 +1,35 @@
+resource "aws_instance" "ec2_example" {
+
+   ami                         =  var.ami_id
+   instance_type               =  var.instance_type
+   count                       =  var.instance_count
+   associate_public_ip_address =  var.enable_public_ip
+   
+   tags = var.project_environment
+}
+
+resource "aws_s3_bucket" "example" {
+  bucket = "terra-dev-mybucket"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
+resource "aws_s3_bucket_ownership_controls" "example" {
+  bucket = aws_s3_bucket.example.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_acl" "example" {
+  depends_on = [aws_s3_bucket_ownership_controls.example]
+
+  bucket = aws_s3_bucket.example.id
+  acl    = "private"
+}
+
+resource "aws_sns_topic" "user_updates" {
+  name = "terra-updates-mytopic"
+}
